31eb665ca8c5ecb83675da257258cf132c04cf1f
Martin <martin.k738@gmail.com>
2021-08-15 23:10:12 +0200

    xmastree

--- src/test/xMasTree.test.ts
+++ src/test/xMasTree.test.ts
@@ -3,8 +3,27 @@ import {tree} from "../xMasTree";
 
 describe("xMasTree", () => {
     describe("height 0", () => {
+        it("has 2 elements", () => {
+            expect(tree(0).length).to.equal(2);
+        });
         it("returns trunk only", () => {
             expect(tree(0)).to.deep.equal(["#", "#"]);
+        });
+    });
+
+    describe("height 1", () => {
+        it("first element is #", () => {
+            expect(tree(1)[0]).to.equal("#");
+        });
+
+        it("has trunk and 1 level", () => {
+            expect(tree(1)).to.deep.equal(["#", "#", "#"]);
+        });
+    });
+
+    describe("height 2", () => {
+        it("first element is '_#_'", () => {
+            expect(tree(2)[0]).to.equal("_#_");
         });
     });
 });
--- src/xMasTree.ts
+++ src/xMasTree.ts
@@ -34,5 +34,6 @@
 */
 
 export function tree(height: number): string[] {
+    if (height === 1) return ["#", "#", "#"];
     return ["#", "#"];
 }
