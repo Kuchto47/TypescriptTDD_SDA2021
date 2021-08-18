8c443f12c0c40cd2ddc380735304a77f451c8f4e
Martin <martin.k738@gmail.com>
2021-08-18 12:16:05 +0200

    xmasTree forcestop

--- src/test/xMasTree.test.ts
+++ src/test/xMasTree.test.ts
@@ -1,1 +1,24 @@
+import { expect } from "chai";
 import {tree} from "../xMasTree";
+
+describe('xMastreeTest', () => {
+    it('should return ["#", "#"] when input is 0', () => {
+        expect(tree(0)).to.deep.equal(['#', '#']);
+    })
+
+    it('should draw tree when input is 1', () => {
+        expect(tree(1)).to.deep.equal(['#', '#', '#']);
+    })
+
+    it('should draw tree when input is 2', () => {
+        expect(tree(2)).to.deep.equal(['_#_', '###', '_#_', '_#_']);
+    })
+
+    it('should draw tree when input is 3', () => {
+        expect(tree(3)).to.deep.equal(['__#__', '_###_', '#####', '__#__', '__#__']);
+    })
+
+    it('should draw tree when input is 4', () => {
+        expect(tree(4)).to.deep.equal(['___#___', '__###__', '_#####_','#######', '___#___', '___#___']);
+    })
+})
--- src/xMasTree.ts
+++ src/xMasTree.ts
@@ -18,9 +18,40 @@
     #####          ----->  3 - Height of Tree
     __#__           1
     __#__           2 - Trunk/Stem of Tree
+
+    tree(0) -> ['#', '#']
+    tree(1) -> ['#', '#', '#']
 * PLACEHOLDER for testing ideas if requested...
 */
 
-export function tree() {
-    
+export function tree(height: number): string[] {
+    if (height == 0) {
+        return ['#', '#'];
+    }
+    let treeCrown: string[] = [];
+    let maxWidth = 2 * height - 1;
+    for (let i=0; i<height; i++) {
+        treeCrown.push(`${"_".repeat(height - i - 1)}${"#".repeat(1 + 2*i)}${"_".repeat(height - i - 1)}`);
+    }
+
+    let trunkLevel = `${"_".repeat(height-1)}#${"_".repeat(height-1)}`;
+    let trunk = [trunkLevel,trunkLevel];
+    // if (height == 1) {
+    //     treeCrown = ['#'];
+       
+    // }
+    // if (height == 2){
+    //     treeCrown = ['_#_', '###'];
+        
+    // }
+
+    // if (height == 3){
+    //     treeCrown = ['__#__', '_###_', '#####'];
+       
+    // }
+    // if(height == 4){
+    //     treeCrown = ['___#___', '__###__', '_#####_','#######'];
+    // }
+
+    return treeCrown.concat(trunk);
 }
